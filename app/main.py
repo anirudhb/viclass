bc211369617668abc4644f02592d4960e71c47a6
abdulhannanm <abdulhannanm85@gmail.com>
2022-12-30 14:46:14 -0800

    finished form

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from typing import Union
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form, File, UploadFile
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.datastructures import URL
+
 
 app = FastAPI()
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -19,6 +22,26 @@ def home(request: Request):
 def create(request: Request):
     return templates.TemplateResponse("generic.html", {"request": request})
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("items.html", {"request": request, "id": id})
+
+@app.get('/prof')
+def profform(request: Request):
+    return templates.TemplateResponse("prof.html", {"request": request})
+
+
+
+
+@app.post("/submitform", response_class=RedirectResponse, status_code=302)
+async def handle_form(video_title: str = Form(...), url: str = Form(...), profname: str = Form(...)):
+    print(video_title)
+    print(url)
+    print(profname)
+    return '/video/'+profname+"/"+video_title+"/"+url
+
+@app.get("/video/{profname}/{video_title}/{url}", response_class=HTMLResponse)
+async def read_item(request: Request, profname: str,video_title: str, url: str):
+    return templates.TemplateResponse("video.html", {"request": request, "video_title": video_title, "profname": profname, "url": url})
+    
+
+
+if __name__ == "__main__":
+    uvicorn.run("fastapi_code:app")
