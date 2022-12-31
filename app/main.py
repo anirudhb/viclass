d4e58dc7c3fdb466f6d08d963b699f8b06dee894
abdulhannanm <abdulhannanm85@gmail.com>
2022-12-30 17:23:50 -0800

    finished chat

--- app/main.py
+++ app/main.py
@@ -15,70 +15,33 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+from fastapi import FastAPI
 
-@app.get("/home")
-def get_home(request: Request):
-    return templates.TemplateResponse("homechat.html", {"request": request})
+app = FastAPI()
 
-@app.get("/chat")
-def get_chat(request: Request):
-    return templates.TemplateResponse("chatchat.html", {"request": request})
-
-
-@app.get("/api/current_user")
-def get_user(request: Request):
-    return request.cookies.get("X-Authorization")
-
-
-class RegisterValidator(BaseModel):
-    username: str
-
-    class Config:
-        orm_mode = True
-
-
-@app.post("/api/register")
-def register_user(user: RegisterValidator, response: Response):
-    response.set_cookie(key="X-Authorization", value=user.username, httponly=True)
-
-
-class SocketManager:
+class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[(WebSocket, str)] = []
+        self.connections: List[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket, user: str):
+    async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append((websocket, user))
+        self.connections.append(websocket)
 
-    def disconnect(self, websocket: WebSocket, user: str):
-        self.active_connections.remove((websocket, user))
-
-    async def broadcast(self, data: dict):
-        for connection in self.active_connections:
-            await connection[0].send_json(data)
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
 
 
-manager = SocketManager()
+manager = ConnectionManager()
 
 
-@app.websocket("/api/chat")
-async def chat(websocket: WebSocket):
-    sender = websocket.cookies.get("X-Authorization")
-    if sender:
-        await manager.connect(websocket, sender)
-        response = {
-            "sender": sender,
-            "message": "got connected"
-        }
-        await manager.broadcast(response)
-        try:
-            while True:
-                data = await websocket.receive_json()
-                await manager.broadcast(data)
-        except WebSocketDisconnect:
-            manager.disconnect(websocket, sender)
-            response['message'] = "left"
-            await manager.broadcast(response)
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    while True:
+        data = await websocket.receive_text()
+        await manager.broadcast(f"Client {client_id}: {data}")
+
 
 
 
